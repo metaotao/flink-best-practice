379326a03acb3ec3b270df36ec4e0562e45374a5
jy.tao <215330435tjy@gmail.com>
2020-09-05 20:26:35 +0800

    add FraudDetector

---
+++ src/main/java/com/zhiwei/flink/practice/frauddetection/FraudDetector.java
@@ -0,0 +1,26 @@
+package com.zhiwei.flink.practice.frauddetection;
+
+import org.apache.flink.walkthrough.common.entity.Alert;
+import org.apache.flink.walkthrough.common.entity.Transaction;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.util.Collector;
+
+public class FraudDetector extends KeyedProcessFunction<Long, Transaction, Alert> {
+    private static final long serialVersionUID = 1L;
+
+    private static final double SMALL_AMOUNT = 1.00;
+    private static final double LARGE_AMOUNT = 500.00;
+    private static final long ONE_MINUTE = 60 * 1000;
+
+    @Override
+    public void processElement(
+            Transaction transaction,
+            Context context,
+            Collector<Alert> collector) throws Exception {
+
+        Alert alert = new Alert();
+        alert.setId(transaction.getAccountId());
+
+        collector.collect(alert);
+    }
+}
