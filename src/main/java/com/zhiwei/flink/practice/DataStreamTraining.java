4a60a183c95fcff810549e20132a893edf4b5256
jy.tao <215330435tjy@gmail.com>
2020-10-09 23:53:19 +0800

    flink window traning

---
+++ src/main/java/com/zhiwei/flink/practice/DataStreamTraining.java
@@ -0,0 +1,9 @@
+package com.zhiwei.flink.practice;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DataStreamTraining {
+    private static List<Integer> data = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+}
