99449b1b70d2c025c8aff07bc4e4f46ffb2a4bac
jy.tao <215330435tjy@gmail.com>
2020-10-11 23:46:23 +0800

    update datastream

--- src/main/java/com/zhiwei/flink/practice/DataStreamTraining.java
+++
@@ -1,9 +0,0 @@
-package com.zhiwei.flink.practice;
-
-import java.util.Arrays;
-import java.util.List;
-
-public class DataStreamTraining {
-    private static List<Integer> data = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-
-}
