ac40e2e15ff35d876f86181a43710676ddc70601
jy.tao <215330435tjy@gmail.com>
2020-10-30 23:45:54 +0800

    update

--- src/main/java/com/zhiwei/flink/practice/tablesql/RidePerHour.java
+++ src/main/java/com/zhiwei/flink/practice/tablesql/RidePerHour.java
@@ -1,11 +1,23 @@
 package com.zhiwei.flink.practice.tablesql;
 
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 
 public class RidePerHour {
     public static void main(String[] args) {
         ParameterTool parameterTool = ParameterTool.fromArgs(args);
         String input = parameterTool.getRequired("input");
+        final int maxEventDelay = 60;       	// events are out of order by max 60 seconds
+        final int servingSpeedFactor = 1800; 	// events of 30 minutes are served in 1 second
+
+        // set up streaming execution environment
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+
+        // create a TableEnvironment
+        StreamTableEnvironment tEnv = StreamTableEnvironment.create(env);
 
     }
 }
