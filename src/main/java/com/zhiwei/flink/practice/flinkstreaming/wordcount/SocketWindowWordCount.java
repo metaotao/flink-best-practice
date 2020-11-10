ae33b17dd0397f5d03525c8bcd03e1d689da15df
jy.tao <215330435tjy@gmail.com>
2020-11-10 23:34:24 +0800

    update some function to 1.11.1

--- src/main/java/com/zhiwei/flink/practice/flinkstreaming/wordcount/SocketWindowWordCount.java
+++ src/main/java/com/zhiwei/flink/practice/flinkstreaming/wordcount/SocketWindowWordCount.java
@@ -5,8 +5,6 @@ import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
-import org.apache.flink.util.Collector;
 
 public class SocketWindowWordCount {
     public static void main(String[] args) throws Exception {
