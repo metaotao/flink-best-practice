882905bfd80e61017588dcdcdf7bd06e43239241
jy.tao <215330435tjy@gmail.com>
2020-11-27 23:50:44 +0800

    add process function

--- src/main/java/com/zhiwei/flink/practice/demo/HotItems.java
+++ src/main/java/com/zhiwei/flink/practice/demo/HotItems.java
@@ -1,6 +1,12 @@
 package com.zhiwei.flink.practice.demo;
 
 import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.api.java.tuple.Tuple1;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
 
 /**
  * 计算实时热门商品
@@ -10,7 +16,30 @@ public class HotItems {
 
     }
 
+    public static class TopNHotItems extends KeyedProcessFunction<Tuple, ItemViewCount, String> {
+        @Override
+        public void processElement(ItemViewCount value, Context ctx, Collector<String> out) throws Exception {
 
+        }
+    }
+
+        /** 用于输出窗口的结果 */
+    public static class WindowResultFunction implements WindowFunction<Long, ItemViewCount, Tuple, TimeWindow> {
+
+        @Override
+        public void apply(
+                Tuple key,  // 窗口的主键，即 itemId
+                TimeWindow window,  // 窗口
+                Iterable<Long> aggregateResult, // 聚合函数的结果，即 count 值
+                Collector<ItemViewCount> collector  // 输出类型为 ItemViewCount
+        ) {
+            Long itemId = ((Tuple1<Long>) key).f0;
+            Long count = aggregateResult.iterator().next();
+            collector.collect(ItemViewCount.of(itemId, window.getEnd(), count));
+        }
+    }
+
+    /** COUNT 统计的聚合函数实现，每出现一条记录加一 */
     public static class CountAgg implements AggregateFunction<UserBehavior,Long,Long> {
 
         @Override
