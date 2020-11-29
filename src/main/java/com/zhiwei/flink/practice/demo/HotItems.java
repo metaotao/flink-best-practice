e241f61325a34c9a9257acdd134f9e77f3ef5b33
Flora Tao <215330435tjy@gmail.com>
2020-11-29 21:53:07 +0800

    add hotItems demo

--- src/main/java/com/zhiwei/flink/practice/demo/HotItems.java
+++ src/main/java/com/zhiwei/flink/practice/demo/HotItems.java
@@ -1,30 +1,138 @@
 package com.zhiwei.flink.practice.demo;
 
+import org.apache.flink.api.common.eventtime.AscendingTimestampsWatermarks;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.state.ListState;
+import org.apache.flink.api.common.state.ListStateDescriptor;
+import org.apache.flink.api.java.io.PojoCsvInputFormat;
 import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.api.java.tuple.Tuple1;
+import org.apache.flink.api.java.typeutils.PojoTypeInfo;
+import org.apache.flink.api.java.typeutils.TypeExtractor;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
 import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
+import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.sql.Timestamp;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * 计算实时热门商品
  */
 public class HotItems {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        // 按照EventTime处理
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        env.setParallelism(1);
+
+        URL fileUrl = HotItems.class.getClassLoader().getResource("UserBehavior.csv");
+        Path filePath = Path.fromLocalFile(new File(fileUrl.toURI()));
+
+        PojoTypeInfo<UserBehavior> pojoType = (PojoTypeInfo<UserBehavior>) TypeExtractor.createTypeInfo(UserBehavior.class);
+        //由于java反射抽取的字段顺序是不确定的，所以需要显示的指定字段顺序
+        String[] fieldOrder = new String[]{"userId", "itemId", "categoryId", "behavior", "timestamp"};
+
+        //创建PojoCsvInputFormat
+        PojoCsvInputFormat<UserBehavior> csvInput = new PojoCsvInputFormat<>(filePath, pojoType, fieldOrder);
+
+        env
+                //创建数据源，得到 UserBehavior 类型的DataStream
+                .createInput(csvInput, pojoType)
+                .assignTimestampsAndWatermarks(new AscendingTimestampExtractor<UserBehavior>() {
+                    @Override
+                    public long extractAscendingTimestamp(UserBehavior element) {
+
+                        //原始数据单位毫秒，转成秒
+                        return element.timestamp * 1000;
+                    }
+                })
+                .filter((FilterFunction<UserBehavior>) userBehavior -> {
+
+                    // 过滤出只有点击的数据
+                    return userBehavior.behavior.equals("pv");
+                })
+                .keyBy("itemId")
+                .timeWindow(Time.minutes(60), Time.minutes(5))
+                .aggregate(new CountAgg(), new WindowResultFunction())
+                .keyBy("windowEnd")
+                .process(new TopNHotItems(3))
+                .print();
+
+        env.execute("Hot Items Job");
 
     }
 
+    /** 求某个窗口中前 N 名的热门点击商品，key 为窗口时间戳，输出为 TopN 的结果字符串 */
     public static class TopNHotItems extends KeyedProcessFunction<Tuple, ItemViewCount, String> {
         private final int topSize;
+
+        // 用于存储商品和点击数的状态。待收齐同一个窗口的数据后，再触发 TopN 计算
+        private ListState<ItemViewCount> itemState;
 
         public TopNHotItems(int topSize) {
             this.topSize = topSize;
         }
+
+        @Override
+        public void open(Configuration parameters) throws Exception {
+            super.open(parameters);
+            // 状态的注册
+            ListStateDescriptor<ItemViewCount> itemsStateDesc = new ListStateDescriptor<>(
+                    "itemState-state",
+                    ItemViewCount.class);
+            itemState = getRuntimeContext().getListState(itemsStateDesc);
+        }
         @Override
         public void processElement(ItemViewCount value, Context ctx, Collector<String> out) throws Exception {
+            itemState.add(value);
+            // 注册 windowEnd+1 的 EventTime Timer, 当触发时，说明收齐了属于windowEnd窗口的所有商品数据
+            ctx.timerService().registerEventTimeTimer(value.windowEnd + 1);
+        }
 
+        @Override
+        public void onTimer(
+                long timestamp, OnTimerContext ctx, Collector<String> out) throws Exception {
+            // 获取收到的所有商品点击量
+            List<ItemViewCount> allItems = new ArrayList<>();
+            for (ItemViewCount item : itemState.get()) {
+                allItems.add(item);
+            }
+            // 提前清除状态中的数据，释放空间
+            itemState.clear();
+            // 按照点击量从大到小排序
+            allItems.sort((o1, o2) -> (int) (o2.viewCount - o1.viewCount));
+            // 将排名信息格式化成 String, 便于打印
+            StringBuilder result = new StringBuilder();
+            result.append("====================================\n");
+            result.append("时间: ").append(new Timestamp(timestamp-1)).append("\n");
+            for (int i=0;i<topSize;i++) {
+                ItemViewCount currentItem = allItems.get(i);
+                // No1:  商品ID=12224  浏览量=2413
+                result.append("No").append(i).append(":")
+                        .append("  商品ID=").append(currentItem.itemId)
+                        .append("  浏览量=").append(currentItem.viewCount)
+                        .append("\n");
+            }
+            result.append("====================================\n\n");
+
+            out.collect(result.toString());
         }
     }
 
@@ -69,9 +177,9 @@ public class HotItems {
     }
     /** 商品点击量(窗口操作的输出类型) */
     public static class ItemViewCount {
-        private long itemId;     // 商品ID
-        private long windowEnd;  // 窗口结束时间戳
-        private long viewCount;  // 商品的点击量
+        public long itemId;     // 商品ID
+        public long windowEnd;  // 窗口结束时间戳
+        public long viewCount;  // 商品的点击量
 
         public static ItemViewCount of(long itemId, long windowEnd, long viewCount) {
             ItemViewCount result = new ItemViewCount();
@@ -84,10 +192,10 @@ public class HotItems {
 
     /** 用户行为数据结构 **/
     public static class UserBehavior {
-        private long userId;         // 用户ID
-        private long itemId;         // 商品ID
-        private int categoryId;      // 商品类目ID
-        private String behavior;     // 用户行为, 包括("pv", "buy", "cart", "fav")
-        private long timestamp;      // 行为发生的时间戳，单位秒
+        public long userId;         // 用户ID
+        public long itemId;         // 商品ID
+        public int categoryId;      // 商品类目ID
+        public String behavior;     // 用户行为, 包括("pv", "buy", "cart", "fav")
+        public long timestamp;      // 行为发生的时间戳，单位秒
     }
 }
