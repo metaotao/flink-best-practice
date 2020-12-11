5b3b20f9dcd6385046f6ec8777b9f0bcd2bb3ff7
Flora Tao <215330435tjy@gmail.com>
2020-12-11 23:19:43 +0800

    [WIP]: taxi ride

--- src/main/java/com/zhiwei/flink/practice/tablesql/source/TaxiRideGenerator.java
+++ src/main/java/com/zhiwei/flink/practice/tablesql/source/TaxiRideGenerator.java
@@ -3,6 +3,8 @@ package com.zhiwei.flink.practice.tablesql.source;
 import com.zhiwei.flink.practice.tablesql.datatypes.TaxiRide;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 
+import java.util.PriorityQueue;
+
 public class TaxiRideGenerator implements SourceFunction<TaxiRide> {
     public static final int SLEEP_MILLIS_PER_EVENT = 10;
     private static final int BATCH_SIZE = 5;
@@ -10,10 +12,15 @@ public class TaxiRideGenerator implements SourceFunction<TaxiRide> {
     @Override
     public void run(SourceContext<TaxiRide> ctx) throws Exception {
 
+        // 定义优先级队列
+        PriorityQueue<TaxiRide> endEventQ = new PriorityQueue<>(100);
+        long id = 0;
+        long maxStartTime = 0;
+
     }
 
     @Override
     public void cancel() {
-
+        running = false;
     }
 }
