2ce892e3b1c50e1c3000d90223ff7e4b1b52f445
Flora Tao <215330435tjy@gmail.com>
2020-12-30 23:45:03 +0800

    update Deprecated function to 1.12

--- src/main/java/com/zhiwei/flink/practice/tablesql/example/PopularPlacesTableApi.java
+++ src/main/java/com/zhiwei/flink/practice/tablesql/example/PopularPlacesTableApi.java
@@ -10,6 +10,8 @@ import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import org.apache.flink.types.Row;
 
+import static org.apache.flink.table.api.Expressions.$;
+
 public class PopularPlacesTableApi {
     public static void main(String[] args) throws Exception {
 
@@ -42,24 +44,25 @@ public class PopularPlacesTableApi {
 
 
         Table popPlaces = tEnv
+                .from("TaxiRides")
                 // scan TaxiRides table
-                .scan("TaxiRides")
                 // filter for valid rides
-                .filter("isInNYC(startLon, startLat) && isInNYC(endLon, endLat)")
+                .filter($("isInNYC(startLon, startLat) && isInNYC(endLon, endLat)"))
                 // select fields and compute grid cell of departure or arrival coordinates
-                .select("eventTime, " +
+                .select($("eventTime, " +
                         "isStart, " +
-                        "(isStart = true).?(toCellId(startLon, startLat), toCellId(endLon, endLat)) AS cell")
+                        "(isStart = true).?(toCellId(startLon, startLat), toCellId(endLon, endLat)) AS cell"))
                 // specify sliding window over 15 minutes with slide of 5 minutes
-                .window(Slide.over("15.minutes").every("5.minutes").on("eventTime").as("w"))
+                .window(Slide.over($("15.minutes")).every($("5.minutes"))
+                        .on($("eventTime")).as("w"))
                 // group by cell, isStart, and window
-                .groupBy("cell, isStart, w")
+                .groupBy($("cell, isStart, w"))
                 // count departures and arrivals per cell (location) and window (time)
-                .select("cell, isStart, w.start AS start, w.end AS end, count(isStart) AS popCnt")
+                .select($("cell, isStart, w.start AS start, w.end AS end, count(isStart) AS popCnt"))
                 // filter for popular places
-                .filter("popCnt > 20")
+                .filter($("popCnt > 20"))
                 // convert cell back to coordinates
-                .select("toCoords(cell) AS location, start, end, isStart, popCnt");
+                .select($("toCoords(cell) AS location, start, end, isStart, popCnt"));
 
         // convert Table into an append stream and print it
         tEnv.toAppendStream(popPlaces, Row.class).print();
