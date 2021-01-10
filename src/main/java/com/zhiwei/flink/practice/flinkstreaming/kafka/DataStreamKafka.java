903c43f1cfc172bb052a3a55a7102504c43bf24f
Flora Tao <215330435tjy@gmail.com>
2021-01-10 20:29:35 +0800

    add source and sink kafka

--- src/main/java/com/zhiwei/flink/practice/flinkstreaming/kafka/DataStreamKafka.java
+++ src/main/java/com/zhiwei/flink/practice/flinkstreaming/kafka/DataStreamKafka.java
@@ -2,15 +2,13 @@ package com.zhiwei.flink.practice.flinkstreaming.kafka;
 
 import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.watermark.Watermark;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
 import org.apache.flink.streaming.connectors.kafka.KafkaSerializationSchema;
 
-import javax.annotation.Nullable;
 import java.time.Duration;
 import java.util.Properties;
 
@@ -27,21 +25,11 @@ public class DataStreamKafka {
                 .addSource(new FlinkKafkaConsumer<>("user_behavior_input",
                 new UserBehaviorDeSerializer(), inputProperties))
                 .assignTimestampsAndWatermarks(WatermarkStrategy.<UserBehavior>forBoundedOutOfOrderness(Duration.ZERO)
-
-                .withTimestampAssigner(new SerializableTimestampAssigner<UserBehavior>() {
-                    private long currentTimestamp = Long.MIN_VALUE;
-
-                    @Override
-                    public long extractTimestamp(UserBehavior o, long l) {
-                        this.currentTimestamp = o.getTimestamp();
-                        return o.getTimestamp();
-                    }
-                    @Nullable
-                    public Watermark getCurrentWatermark() {
-                        return new Watermark(
-                                currentTimestamp == Long.MIN_VALUE ? Long.MIN_VALUE : currentTimestamp - 1);
-                    }
-                }));
+                .withTimestampAssigner((SerializableTimestampAssigner<UserBehavior>) (o, l) -> o.getTimestamp())
+                ).filter((FilterFunction<UserBehavior>) userBehavior -> {
+                    // 过滤出只有点击的数据
+                    return userBehavior.behavior.equals("pv");
+                });
 
         Properties outputProperties = new Properties();
         outputProperties.setProperty("bootstrap.servers", "localhost:9092");
