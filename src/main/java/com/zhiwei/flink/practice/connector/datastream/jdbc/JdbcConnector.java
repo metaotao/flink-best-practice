e927c56014d42349544f5c1d9c63129a36cee3e2
Flora Tao <215330435tjy@gmail.com>
2021-01-17 23:19:40 +0800

    update

--- src/main/java/com/zhiwei/flink/practice/connector/datastream/jdbc/JdbcConnector.java
+++ src/main/java/com/zhiwei/flink/practice/connector/datastream/jdbc/JdbcConnector.java
@@ -4,32 +4,11 @@ import org.apache.flink.connector.jdbc.internal.JdbcBatchingOutputFormat;
 import org.apache.flink.connector.jdbc.internal.options.JdbcOptions;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
-import java.sql.Types;
 
 public class JdbcConnector {
 
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        JdbcBatchingOutputFormat jdbcOutputFormat =
-                JdbcBatchingOutputFormat.builder()
-                        .setOptions(
-                                JdbcOptions.builder()
-                                        .setDBUrl("")
-                                        .setTableName("")
-                                        .build())
-                        .setFieldNames(new String[] {"id", "title", "author", "price", "qty"})
-                        .setFieldTypes(
-                                new int[] {
-                                        Types.INTEGER,
-                                        Types.VARCHAR,
-                                        Types.VARCHAR,
-                                        Types.DOUBLE,
-                                        Types.INTEGER
-                                })
-                        .setKeyFields(null)
-                        .build();
-
-        env.execute();
 
     }
 }
