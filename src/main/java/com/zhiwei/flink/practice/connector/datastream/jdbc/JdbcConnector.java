9ba1ec8eed54857eeb69fbb365ef08a1e0770c84
Flora Tao <215330435tjy@gmail.com>
2021-01-17 23:19:52 +0800

    update

--- src/main/java/com/zhiwei/flink/practice/connector/datastream/jdbc/JdbcConnector.java
+++ src/main/java/com/zhiwei/flink/practice/connector/datastream/jdbc/JdbcConnector.java
@@ -1,7 +1,5 @@
 package com.zhiwei.flink.practice.connector.datastream.jdbc;
 
-import org.apache.flink.connector.jdbc.internal.JdbcBatchingOutputFormat;
-import org.apache.flink.connector.jdbc.internal.options.JdbcOptions;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 
