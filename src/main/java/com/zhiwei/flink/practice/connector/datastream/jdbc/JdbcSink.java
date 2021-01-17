9cf71be66d44baf15a7b41a8a960335cce596ee3
Flora Tao <215330435tjy@gmail.com>
2021-01-17 23:20:15 +0800

    rename

--- src/main/java/com/zhiwei/flink/practice/connector/datastream/jdbc/JdbcSink.java
+++ src/main/java/com/zhiwei/flink/practice/connector/datastream/jdbc/JdbcSink.java
@@ -2,6 +2,6 @@ package com.zhiwei.flink.practice.connector.datastream.jdbc;
 
 import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
 
-public class MyJdbcSink extends RichSinkFunction<SensorReading> {
+public class JdbcSink extends RichSinkFunction<SensorReading> {
 
 }
