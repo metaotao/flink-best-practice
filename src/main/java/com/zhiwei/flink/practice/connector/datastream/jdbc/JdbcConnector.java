181609a029cf4b510b686559dc38d04b27db4093
Flora Tao <215330435tjy@gmail.com>
2021-02-18 23:41:34 +0800

    delete flink top n and add flink batch demo

--- src/main/java/com/zhiwei/flink/practice/connector/datastream/jdbc/JdbcConnector.java
+++ src/main/java/com/zhiwei/flink/practice/connector/datastream/jdbc/JdbcConnector.java
@@ -5,7 +5,7 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 public class JdbcConnector {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
     }
--- src/main/java/com/zhiwei/flink/practice/demo/FlinkBatchDemo.java
+++ src/main/java/com/zhiwei/flink/practice/demo/FlinkBatchDemo.java
@@ -1,9 +1,8 @@
 package com.zhiwei.flink.practice.demo;
 
-public class FlinkTopN {
+public class FlinkBatchDemo {
 
     public static void main(String[] args) {
-
 
     }
 }
